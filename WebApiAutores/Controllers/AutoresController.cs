c6839fe46d65460f980cb8dacfbc1390542e758e
DesarrolloProsis <rmendoza@grupo-prosis.com>
2022-02-25 14:16:58 -0600

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    // GET: AutoresController
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult <List<Autor>> Get()
+        {
+            return new List<Autor>() { 
+                new Autor() { Id = 1 , Nombre = "Rodrigo"},
+                new Autor() { Id = 2 , Nombre = "Alejandro"}
+               };
+        }          
+    }
+}
+ 
