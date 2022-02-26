9d61dfa0be3c38df44ee880d5f7158724523b0ca
DesarrolloProsis <rmendoza@grupo-prosis.com>
2022-02-25 19:02:57 -0600

    Todo bien hasta la clase 24

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -31,6 +31,33 @@ namespace WebApiAutores.Controllers
             await context.SaveChangesAsync();
             return Ok();
         }
+
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if (autor == null) { return BadRequest("el id esta vacio"); }
+            if (autor.Id != id) { return BadRequest("el id esta vacio"); }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe) 
+            {
+                return NotFound();            
+            }
+
+            context.Remove(new Autor { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
  
