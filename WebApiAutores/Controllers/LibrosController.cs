e6030b7275c944dae6ff1e1eed090259393ebcb4
DesarrolloProsis <rmendoza@grupo-prosis.com>
2022-03-01 18:56:33 -0600

    Todo bien has la clase 24

---
+++ WebApiAutores/Controllers/LibrosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Libroes
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            return await _context.Libros.Include(x => x.Autor).FirstOrDefaultAsync(x => x.id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Libro libro)
+        {
+            var existeAutor = await _context.Autores.AnyAsync(x => x.Id == libro.Autorid);
+
+            if (!existeAutor)
+            {
+              return BadRequest($"No existe el autor de Id: {libro.Autorid}"); 
+            }
+
+            _context.Add(libro);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ WebApiAutores/Entidades/Libro.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Libro
+    {
+        public int id { get; set; }
+        public string Titulo { get; set; }
+        public int Autorid { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
